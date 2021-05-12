2cc4d637588df4a0ee395636cd71097bbb193714
TiaGoiNsaNy <tiagoinsany@gmail.com>
2021-05-12 16:46:42 -0300

    :star: Adicionado rota de convite da star

--- routers/redirect.controller.js
+++ routers/redirect.controller.js
@@ -1,6 +1,10 @@
 const { Router } = require("express");
 const router = Router();
 
+router.get("/add", (request, response) => {
+    return response.redirect("https://discord.com/oauth2/authorize?client_id=719524114536333342&scope=bot&permissions=805432446");
+});
+
 router.get("/github", (request, response) => {
     return response.redirect("https://github.com/iNsaNyDevelopers");
 });
